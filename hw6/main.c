0b9bfb2d0964553f459cf2f5dca1497aec70a813
Marshall Johnson <marshall.johnson2022@u.northwestern.edu>
2022-06-01 18:45:58 -0500

    Completed hw6

---
+++ hw6/main.c
@@ -0,0 +1,252 @@
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include<stdio.h>
+
+// DEVCFG0
+#pragma config DEBUG = OFF // disable debugging
+#pragma config JTAGEN = OFF // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // disable flash write protect
+#pragma config BWP = OFF // disable boot write protect
+#pragma config CP = OFF // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL // use fast frc oscillator with pll
+#pragma config FSOSCEN = OFF // disable secondary oscillator
+#pragma config IESO = OFF // disable switching clocks
+#pragma config POSCMOD = OFF // primary osc disabled
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576 // use largest wdt value
+#pragma config WINDIS = OFF // use non-window mode wdt
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz fast rc internal oscillator
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 00000000 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+
+#define DESIRED_BAUD 230400
+#define SYS_FREQ 48000000ul
+
+#define I2C_WRITE 0b01000000
+#define I2C_READ  0b01000001
+
+
+void ReadUART1(char * message, int maxLength);
+void WriteUART1(const char * string);
+
+void write_pin(unsigned char reg, unsigned char value);
+unsigned char read_pin(unsigned char reg);
+void i2c_master_setup(void);
+void i2c_master_start(void);
+void i2c_master_restart(void);
+void i2c_master_send(unsigned char byte);
+unsigned char i2c_master_recv(void);
+void i2c_master_ack(int val);
+void i2c_master_stop(void);
+
+int main() {
+
+    __builtin_disable_interrupts(); // disable interrupts while initializing things
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    TRISAbits.TRISA4 = 0;
+    TRISBbits.TRISB4 = 1;
+
+    PORTAbits.RA4 = 0;
+    
+    LATAbits.LATA4 = 0;
+    
+    U1RXRbits.U1RXR = 0b0000; // Set A2 to U1RX
+    RPB3Rbits.RPB3R = 0b0001; // Set B3 to U1TX
+    
+    // turn on UART1 without an interrupt
+    U1MODEbits.BRGH = 0; // set baud to NU32_DESIRED_BAUD
+    U1BRG = ((SYS_FREQ / DESIRED_BAUD) / 16) - 1;
+
+    // 8 bit, no parity bit, and 1 stop bit (8N1 setup)
+    U1MODEbits.PDSEL = 0;
+    U1MODEbits.STSEL = 0;
+
+    // configure TX & RX pins as output & input pins
+    U1STAbits.UTXEN = 1;
+    U1STAbits.URXEN = 1;
+    
+    // configure hardware flow control using RTS and CTS
+    U1MODEbits.UEN = 2;
+
+    // enable the uart
+    U1MODEbits.ON = 1;
+    
+    i2c_master_setup();
+    
+    __builtin_enable_interrupts();
+    
+    write_pin(0x00, 0b01111111);
+
+    while (1) {
+        
+        unsigned char r = read_pin(0x09);
+        
+        if (r == 0b00000000) {
+            write_pin(0x0A, 0b10000000);        
+        }
+        else {
+            write_pin(0x0A, 0b00000000);
+        }
+        
+//        unsigned char r = read_pin(0x09);
+//        write_pin(0x0A, 0b00000000);
+        
+        
+        // turn light on
+        LATAbits.LATA4 = 1;
+
+        // delay for 0.5s
+        _CP0_SET_COUNT(0);
+        while (_CP0_GET_COUNT() < 12000000) {}
+
+        // turn light off
+        LATAbits.LATA4 = 0;
+        
+        // delay for 0.5s
+        _CP0_SET_COUNT(0);
+        while (_CP0_GET_COUNT() < 12000000) {}
+    }
+}
+
+// Read from UART1
+// block other functions until you get a '\r' or '\n'
+// send the pointer to your char array and the number of elements in the array
+void ReadUART1(char * message, int maxLength) {
+  char data = 0;
+  int complete = 0, num_bytes = 0;
+  // loop until you get a '\r' or '\n'
+  while (!complete) {
+    if (U1STAbits.URXDA) { // if data is available
+      data = U1RXREG;      // read the data
+      if ((data == '\n') || (data == '\r')) {
+        complete = 1;
+      } else {
+        message[num_bytes] = data;
+        ++num_bytes;
+        // roll over if the array is too small
+        if (num_bytes >= maxLength) {
+          num_bytes = 0;
+        }
+      }
+    }
+  }
+  // end the string
+  message[num_bytes] = '\0';
+}
+
+// Write a character array using UART3
+void WriteUART1(const char * string) {
+  while (*string != '\0') {
+    while (U1STAbits.UTXBF) {
+      ; // wait until tx buffer isn't full
+    }
+    U1TXREG = *string;
+    ++string;
+  }
+}
+
+void write_pin(unsigned char reg, unsigned char value)
+{
+    i2c_master_start();
+    i2c_master_send(I2C_WRITE);
+    i2c_master_send(reg);
+    i2c_master_send(value);
+    i2c_master_stop();
+}
+
+unsigned char read_pin(unsigned char reg)
+{
+    i2c_master_start();
+    i2c_master_send(I2C_WRITE);
+    i2c_master_send(reg);
+    i2c_master_restart();
+    i2c_master_send(I2C_READ);
+    unsigned char r = i2c_master_recv();
+    i2c_master_ack(1);
+    i2c_master_stop();
+    return r;
+}
+
+void i2c_master_setup(void) {
+    // using a large BRG to see it on the nScope, make it smaller after verifying that code works
+    // look up TPGD in the datasheet
+    I2C1BRG = 1000; // I2CBRG = [1/(2*Fsck) - TPGD]*Pblck - 2 (TPGD is the Pulse Gobbler Delay)
+    I2C1CONbits.ON = 1; // turn on the I2C1 module
+}
+
+void i2c_master_start(void) {
+    I2C1CONbits.SEN = 1; // send the start bit
+    while (I2C1CONbits.SEN) {
+        ;
+    } // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {
+    I2C1CONbits.RSEN = 1; // send a restart 
+    while (I2C1CONbits.RSEN) {
+        ;
+    } // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) { // send a byte to slave
+    I2C1TRN = byte; // if an address, bit 0 = 0 for write, 1 for read
+    while (I2C1STATbits.TRSTAT) {
+        ;
+    } // wait for the transmission to finish
+    if (I2C1STATbits.ACKSTAT) { // if this is high, slave has not acknowledged
+        // ("I2C1 Master: failed to receive ACK\r\n");
+        while(1){} // get stuck here if the chip does not ACK back
+    }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C1CONbits.RCEN = 1; // start receiving data
+    while (!I2C1STATbits.RBF) {
+        ;
+    } // wait to receive the data
+    return I2C1RCV; // read and return the data
+}
+
+void i2c_master_ack(int val) { // sends ACK = 0 (slave should send another byte)
+    // or NACK = 1 (no more bytes requested from slave)
+    I2C1CONbits.ACKDT = val; // store ACK/NACK in ACKDT
+    I2C1CONbits.ACKEN = 1; // send ACKDT
+    while (I2C1CONbits.ACKEN) {
+        ;
+    } // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) { // send a STOP:
+    I2C1CONbits.PEN = 1; // comm is complete and master relinquishes bus
+    while (I2C1CONbits.PEN) {
+        ;
+    } // wait for STOP to complete
+}
+
